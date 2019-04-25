b6f477b15d83589bde610f26fcae9152f0d83ead
fengyangcai <fengyangcai@163.com>
2019-04-25 23:51:52 +0800

    修正一下文件hello

--- spring-boot-01-demo/src/main/java/com/caicai/springboot/controller/Hello.java
+++ spring-boot-01-demo/src/main/java/com/caicai/springboot/controller/Hello.java
@@ -9,12 +9,11 @@ public class Hello {
 	@RequestMapping("/test")
 	public String sayHello() {
 		System.out.println("sdsda");
-<<<<<<< HEAD
+
 		
 		System.out.println("你好我要测试一下是否修改了");
-=======
 		System.out.println("哈哈");
->>>>>>> branch 'master' of https://fengyangcai@github.com/fengyangcai/git-eclipse.git
+
 		return "hello springboot 欢迎来到springboot";
 	}
 }
