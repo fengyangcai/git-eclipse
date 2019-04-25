ffdc9f6b8f00c994def68e24abd17f7edb4a88de
fengyangcai <fengyangcai@163.com>
2019-04-25 20:21:00 +0800

    提交hello

--- spring-boot-01-demo/src/main/java/com/caicai/springboot/controller/Hello.java
+++ spring-boot-01-demo/src/main/java/com/caicai/springboot/controller/Hello.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
 
-	@RequestMapping("/sayhello")
+	@RequestMapping("/test")
 	public String sayHello() {
+		System.out.println("sdasdas");
 		return "hello springboot 欢迎来到springboot";
 	}
 }
